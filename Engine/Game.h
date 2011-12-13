b1f6bc2b755c374c123a070c3e738992b0648760
Jody McAdams <jodymcadams@gmail.com>
2011-12-13 00:07:49 -0800

    game engine update

--- Engine/Game.h
+++ Engine/Game.h
@@ -12,14 +12,48 @@
 #include "MathTypes.h"
 #include "OpenGLRenderer.h"
 
+#define GAME_MAX_ART_DESCRIPTIONS 128
+
+typedef struct
+{
+	GLuint			textureFilterMode;
+	GLuint			wrapModeU;
+	GLuint			wrapModeV;
+    RenderMaterial	renderMaterial;
+    u32				renderFlags;
+} MaterialSettings;
+
+typedef struct
+{
+    const char*		textureFileName;
+    ImageType		imageType;
+    GLuint			textureHandle;
+	MaterialSettings	materialSettings;
+    ModelData*		pModelData;
+} ItemArtDescription;
+
 class Game
 {
 public:
-	virtual void Init(OpenGLRenderer* pRenderer) = 0;
+	virtual void Init(OpenGLRenderer* pRenderer);
 	virtual void Update(f32 timeElapsed) = 0;
 	virtual void CleanUp() = 0;
 protected:
 	OpenGLRenderer*  m_pRenderer;
+	
+	void AddArtDescriptionToLoad(ItemArtDescription* pArtDescription);
+	bool WillArtDescriptionBeLoaded(ItemArtDescription* pArtDesc);
+	void LoadItemArt();
+private:
+	ItemArtDescription* m_pLoadedArtDescriptions[GAME_MAX_ART_DESCRIPTIONS];
+    u32 m_numLoadedArtDescriptions;
+    
+    ItemArtDescription* m_pArtDescriptionsToLoadTexturesFor[GAME_MAX_ART_DESCRIPTIONS];
+    u32 m_numArtDescriptionsToLoadTexturesFor;
 };
+
+
+//UTIL
+void DeleteTexture(GLuint* pTextureID);
 
 #endif
