ad1c89ab0ea49df7b8f28c9bcc5b96a4994af91c
Jody McAdams <jodymcadams@gmail.com>
2011-12-21 02:32:02 -0800

    New engine... again?

---
+++ CoreEngine3D(OSX)/CoreEngine3D_OSX_.h
@@ -0,0 +1,13 @@
+//
+//  CoreEngine3D_OSX_.h
+//  CoreEngine3D(OSX)
+//
+//  Created by Jody McAdams on 12/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreEngine3D_OSX_ : NSObject
+
+@end
---
+++ CoreEngine3D(OSX)/CoreEngine3D_OSX_.m
@@ -0,0 +1,23 @@
+//
+//  CoreEngine3D_OSX_.m
+//  CoreEngine3D(OSX)
+//
+//  Created by Jody McAdams on 12/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoreEngine3D_OSX_.h"
+
+@implementation CoreEngine3D_OSX_
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
