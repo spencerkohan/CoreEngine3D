2f8a7bc0407453ca813f83e80af0faaa2606487b
Jody McAdams <jodymcadams@gmail.com>
2012-02-22 19:24:37 -0800

    bug fix? for animating sprites

--- Engine/SpriteSheetAnimator.cpp
+++ Engine/SpriteSheetAnimator.cpp
@@ -50,15 +50,17 @@ bool SpriteSheetAnimator::GetSpriteInfoForFrame(u32 frame, SpriteFrameInfo* pOut
 	const f32 uIncrement = 1.0f/(f32)cellsPerRow;
 	const f32 vIncrement = 1.0f/(f32)cellsPerRow;
 	
+	const f32 halfTexel = 0.5f/pSheet->textureSize;
+	
 	pOut_SpriteFrameInfo->pTextureHandle = pSheet->pTextureHandle;
 	pOut_SpriteFrameInfo->cellsPerRow = cellsPerRow;
 	pOut_SpriteFrameInfo->textureOffset.x = uIncrement*(f32)cellX;
-	pOut_SpriteFrameInfo->textureOffset.y = vIncrement*(f32)cellY;
+	pOut_SpriteFrameInfo->textureOffset.y = vIncrement*(f32)cellY+halfTexel;
 	
 	return true;
 }
 
-void SpriteSheetAnimator::SetSpriteSheet(u32 spriteSheetIndex, u32* pTextureHandle, u32 cellsPerRow)
+void SpriteSheetAnimator::SetSpriteSheet(u32 spriteSheetIndex, u32* pTextureHandle, f32 textureSize, u32 cellsPerRow)
 {
 	if(spriteSheetIndex > SPRITESHEET_MAX_SHEETS-1)
 	{
@@ -66,6 +68,7 @@ void SpriteSheetAnimator::SetSpriteSheet(u32 spriteSheetIndex, u32* pTextureHand
 	}
 	
 	SpriteSheet* pSheet = &m_spriteSheets[spriteSheetIndex];
+	pSheet->textureSize = textureSize;
 	pSheet->cellsPerRow = cellsPerRow;
 	pSheet->pTextureHandle = pTextureHandle;
 	pSheet->totalCells = cellsPerRow*cellsPerRow;
