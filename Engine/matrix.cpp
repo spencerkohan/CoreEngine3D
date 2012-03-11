b07f7c037ba216c449633d824ea0850e2c737221
Jody McAdams <jodymcadams@gmail.com>
2012-03-11 11:48:32 -0700

    somehow these matrix changes didn't make it onto the internet

--- Engine/matrix.cpp
+++ Engine/matrix.cpp
@@ -670,6 +670,99 @@ void mat4f_LoadScaledRotationFromUp_IgnoreTranslation(f32* pOut_matrix, const ve
 	pOut_matrix[8] =  mat_left.z;	pOut_matrix[9] =  mat_up.z;	pOut_matrix[10] =  mat_at.z;	pOut_matrix[11] = 0.0f;
 }
 
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+void mat4f_LoadScaledRotationFromLeftAndUp(f32* pOut_matrix, const vec3* pLeft, const vec3* pUp, const f32 scale)
+{
+	vec3 mat_up;
+	CopyVec3(&mat_up,pUp);
+	
+	vec3 mat_left;
+	CopyVec3(&mat_left,pLeft);
+	
+	//Create at
+	vec3 mat_at;
+	CrossVec3(&mat_at, pUp, &mat_left);
+	NormalizeVec3_Self(&mat_at);
+	
+	//Create up
+	CrossVec3(&mat_up, &mat_left, &mat_at);
+	
+	ScaleVec3_Self(&mat_up,scale);
+	ScaleVec3_Self(&mat_at,scale);
+	ScaleVec3_Self(&mat_left,scale);
+	
+	pOut_matrix[0] =  mat_left.x;	pOut_matrix[1] =  mat_up.x;	pOut_matrix[2]  =  mat_at.x;	pOut_matrix[3]  = 0.0f;	
+	pOut_matrix[4] =  mat_left.y;	pOut_matrix[5] =  mat_up.y;	pOut_matrix[6]  =  mat_at.y;	pOut_matrix[7]  = 0.0f;
+	pOut_matrix[8] =  mat_left.z;	pOut_matrix[9] =  mat_up.z;	pOut_matrix[10] =  mat_at.z;	pOut_matrix[11] = 0.0f;
+	pOut_matrix[12] = 0.0f;
+	pOut_matrix[13] = 0.0f;
+	pOut_matrix[14] = 0.0f;
+	pOut_matrix[15] = 1.0f;
+}
+
+
+void mat4f_LoadScaledRotationFromUpAndLeft(f32* pOut_matrix, const vec3* pLeft, const vec3* pUp, const f32 scale)
+{
+	vec3 mat_up;
+	CopyVec3(&mat_up,pUp);
+	
+	vec3 mat_left;
+	CopyVec3(&mat_left,pLeft);
+	
+	//Create at
+	vec3 mat_at;
+	CrossVec3(&mat_at, pUp, &mat_left);
+	NormalizeVec3_Self(&mat_at);
+	
+	//Create left
+	CrossVec3(&mat_left, &mat_at, &mat_up);
+	
+	ScaleVec3_Self(&mat_up,scale);
+	ScaleVec3_Self(&mat_at,scale);
+	ScaleVec3_Self(&mat_left,scale);
+	
+	pOut_matrix[0] =  mat_left.x;	pOut_matrix[1] =  mat_up.x;	pOut_matrix[2]  =  mat_at.x;	pOut_matrix[3]  = 0.0f;	
+	pOut_matrix[4] =  mat_left.y;	pOut_matrix[5] =  mat_up.y;	pOut_matrix[6]  =  mat_at.y;	pOut_matrix[7]  = 0.0f;
+	pOut_matrix[8] =  mat_left.z;	pOut_matrix[9] =  mat_up.z;	pOut_matrix[10] =  mat_at.z;	pOut_matrix[11] = 0.0f;
+	pOut_matrix[12] = 0.0f;
+	pOut_matrix[13] = 0.0f;
+	pOut_matrix[14] = 0.0f;
+	pOut_matrix[15] = 1.0f;
+}
+
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+void mat4f_LoadScaledRotationFromUp(f32* pOut_matrix, const vec3* pUp, const f32 scale)
+{
+	vec3 mat_up;
+	CopyVec3(&mat_up,pUp);
+	
+	vec3 mat_left;
+	CopyVec3(&mat_left,&vec3_left);
+	
+	//Create at
+	vec3 mat_at;
+	CrossVec3(&mat_at, pUp, &mat_left);
+	NormalizeVec3_Self(&mat_at);
+	
+	//Create left
+	CrossVec3(&mat_left, &mat_up, &mat_at);
+	
+	ScaleVec3_Self(&mat_up,scale);
+	ScaleVec3_Self(&mat_at,scale);
+	ScaleVec3_Self(&mat_left,scale);
+	
+	pOut_matrix[0] =  mat_left.x;	pOut_matrix[1] =  mat_up.x;	pOut_matrix[2]  =  mat_at.x;	pOut_matrix[3]  = 0.0f;	
+	pOut_matrix[4] =  mat_left.y;	pOut_matrix[5] =  mat_up.y;	pOut_matrix[6]  =  mat_at.y;	pOut_matrix[7]  = 0.0f;
+	pOut_matrix[8] =  mat_left.z;	pOut_matrix[9] =  mat_up.z;	pOut_matrix[10] =  mat_at.z;	pOut_matrix[11] = 0.0f;
+	pOut_matrix[12] = 0.0f;
+	pOut_matrix[13] = 0.0f;
+	pOut_matrix[14] = 0.0f;
+	pOut_matrix[15] = 1.0f;
+}
+
 
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
